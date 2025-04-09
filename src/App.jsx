1e10689aa310647afcac246614d663a542881620
Deni0012 <deni.dg00@icloud.com>
2025-04-09 10:47:44 +0200

    feat: completed project with comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
+// Importazioni standard in APP
 import { useState } from 'react'
 import './App.css'
 import languages from './data/languages'
+import Buttons from './components/button';
+import Main from './components/Main';
 
+// Creazione della funzione App 
 function App() {
   const [activeLanguages, setActiveLanguages] = useState(0)
   const currentLanguages = languages.find(languages => languages.id === activeLanguages);
 
   return (
     <>
+      <div className="container-central">
+        <div className="container-languages">
+          <h1>Learn Web development</h1>
+
+          {/* // Buttons e Main collegati richiamando le funzioni */}
+          <Buttons languages={languages} activeLanguages={activeLanguages} setActiveLanguages={setActiveLanguages} />
+          <Main currentLanguages={currentLanguages} />
+
+        </div>
+      </div >
 
     </>
   )
 }
 
-export default App
+// Esportazione di App
+export default App;
